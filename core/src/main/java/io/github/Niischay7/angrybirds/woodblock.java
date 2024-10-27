00c5458f58a8b2d8c4ce017c3198b6c7382e9457
Niischay4 <147917000+Niischay7@users.noreply.github.com>
2024-10-27 11:28:29 +0530

    Update woodblock.java

--- core/src/main/java/io/github/Niischay7/angrybirds/woodblock.java
+++ core/src/main/java/io/github/Niischay7/angrybirds/woodblock.java
@@ -6,6 +6,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 public class woodblock extends blocks {
     public woodblock(float size, int hp) {
         super("wood", size, hp);
-        this.texture = new Texture("woodie-removebg-preview.png");
+        this.texture = new Texture("woodie-removebg-preview.png");//only wood image for GUI
     }
 }
